c7857ec8768c97e4da461370e58bc9a89072b0bf
pceprian <paula.ceprian@sdggroup.com>
2026-06-25 16:42:39 +0200

    Update streamlit_app.py
    Making the API more clean

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
-# Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests  
 
 
 # Write directly to the app
@@ -32,10 +32,16 @@ ingredients_list = st.multiselect(
 
 
 if ingredients_list:   
-    ingredients_string = '' #no space between the quotes
 
+    ingredients_string = '' #no space between the quotes
+    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+
+        #New section to display smoothiefroot nutrition information
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #Build a SQL Insert Statement
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -48,12 +54,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered {name_on_order}!', icon="✅")
 
-
-#New section to display smoothiefroot nutrition information
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json()
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
